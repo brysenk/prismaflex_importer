bac14c052ff7ced149abea827d6655b23b71ec83
Brysen Keith <brysenkeith@Brysens-MacBook-Air-3.local>
2017-03-30 13:42:30 -0600

    added scale file incorporation

--- PrismaData.py
+++ PrismaData.py
@@ -52,7 +52,7 @@ try:
     #typesl = [x.lstrip() for x in readlines] 
     types = [line.split(";") for line in readlines]
     pressure_data = types[6:]
-    indexl = [s[0] for s in pressure_data]
+    indexpl = [s[0] for s in pressure_data]
     timel = [s[1] for s in pressure_data]
     accesspl = [s[2] for s in pressure_data]
     accesspl = [s.strip() for s in accesspl]
@@ -67,7 +67,7 @@ try:
     db = None
     db = psycopg2.connect("dbname='crrt' user='brysenkeith' host='localhost' host='/tmp/'")
     cur = db.cursor()        
-    cur.execute("""INSERT INTO pressures (index, time, access_p, filter_p, effluent_p, return_p, arps) VALUES (%(text)s, %(text)s, %(text)s, %(text)s, %(text)s, %(text)s, %(text)s);""", (indexl, timel, accesspl, filterpl, efflpl, returnpl, arpspl))
+    cur.execute("""INSERT INTO pressures (index, time, access_p, filter_p, effluent_p, return_p, arps) VALUES (%s, %s, %s, %s, %s, %s, %s);""", (indexl, timel, accesspl, filterpl, efflpl, returnpl, arpspl))
     #cur.execute("""INSERT INTO pressures (time) VALUES (%s);""", (timel,))
     db.commit()
 except psycopg2.DatabaseError, e:
@@ -84,19 +84,71 @@ finally:
         db.close()
 #%%
 #works for one file, need to iterate over multiple
-"""
+
 #for i in range(len(list_of_E)):
 lines = open(list_of_E[0], 'rb')
 readlines = lines.readlines()
 types = [line.split(";") for line in readlines]
 calibration_data = types[0:18]
-event_data = types[19:]
-db = psycopg2.connect("dbname='crrt' user='postgres' host='localhost' host='/tmp/' password='keith1968'")
-cur = db.cursor()
-cur.execute("INSERT INTO events (index) VALUES (%s);", (event_data[:][0])) 
+event_data = types[20:]
+indexE = [s[0] for s in event_data]
+timeE = [s[1] for s in event_data]
+classcodE = [s[2] for s in event_data]
+classE = [s[3] for s in event_data]
+typecodE = [s[4] for s in event_data]
+typeE = [s[5] for s in event_data]
+samplecodE = [s[6] for s in event_data]
+sampleE = [s[7] for s in event_data]
+fluidE = [s[8] for s in event_data]
 
 """
+db = psycopg2.connect("dbname='crrt' user='brysenkeith' host='localhost' host='/tmp/'")
+cur = db.cursor()
+cur.execute("INSERT INTO events (index) VALUES (%s);", (event_data)) 
+"""
 
+#%%
+#Import scale files (S.txt) into PSQL database
+
+try:
+    lines = open(list_of_S[0], 'rb')
+    readlines = lines.readlines()
+    types = [line.split(";") for line in readlines]
+    scale_data = types[6:]
+    indexS = [s[0] for s in scale_data]
+    timeS = [s[1] for s in scale_data]
+    runtimeS = [s[2] for s in scale_data]
+    postinfS = [s[3] for s in scale_data]
+    preinfS = [s[4] for s in scale_data]
+    dialysateS = [s[5] for s in scale_data]
+    effluentS = [s[6] for s in scale_data]
+    prebloodinfS = [s[7] for s in scale_data]
+    syringeinfS = [s[8] for s in scale_data]
+    excessfluidS = [s[9] for s in scale_data]
+    pumpone = [s[10] for s in scale_data]
+    pumptwo = [s[11] for s in scale_data]
+    pumpthree = [s[12] for s in scale_data]
+    pumpfour = [s[13] for s in scale_data]
+    db = None
+    db = psycopg2.connect("dbname='crrt' user='brysenkeith' host='localhost' host='/tmp/'")
+    cur = db.cursor()        
+    cur.execute("""INSERT INTO pressures (index, time, access_p, filter_p, effluent_p, return_p, arps) VALUES (%s, %s, %s, %s, %s, %s, %s);""", (indexl, timel, accesspl, filterpl, efflpl, returnpl, arpspl))
+    #cur.execute("""INSERT INTO pressures (time) VALUES (%s);""", (timel,))
+    db.commit()
+except psycopg2.DatabaseError, e:
+    
+    if db:
+        db.rollback()
+    
+    print 'Error %s' % e    
+    sys.exit(1)
+    
+    
+finally:
+    if db:
+        db.close()
+        
+        
 """
 psuedocode for python sql import
 GOAL: have 4 tables of the P,S,E,C-stripped off of E format
